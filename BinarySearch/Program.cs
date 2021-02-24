cf531cb40c2cfad7bfec9d52f284e8b983f9e280
Poul Henriksen <pohe@easj.dk>
2021-02-24 10:00:52 +0100

    Add project files.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            Console.WriteLine("Enter number to search?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Search s = new Search();
+            Console.WriteLine("index no. is {0}", s.LinearSearch(myArray, myArray.Length - 1,number ));
+            Console.WriteLine("index no. is {0}", s.binarySearch(myArray, 0, myArray.Length - 1, number));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BinarySearch/Search.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearch
+{
+    public class Search
+    {
+
+        public int LinearSearch(int[] myArray, int n, int x)
+        {
+            //implementer metoden
+            return -1;
+        }
+
+        public int binarySearch(int[] myArray, int l, int r, int x)
+
+        {
+            //implementer metoden
+            return -1;
+        }
+    }
+    
+}
